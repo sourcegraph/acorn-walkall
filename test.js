745d6c1678536ce98f4fefdc4b75fd460e99cba7
Graeme Yeates <megawac@gmail.com>
2015-03-26 03:02:55 -0400

    Add support for Arrows and ObjectPatterns

--- test.js
+++ test.js
@@ -2,13 +2,20 @@ var walkall = require('./walkall');
 var acorn = require('acorn'), should = require('should'), walk = require('acorn/util/walk');
 
 describe('walkall', function() {
+  var code = 'import a from "b";var foo = function(a) {}, bar = {d: 7}; let {d} = bar; export function foo() {} [1,2].map(x => x)';
   it('walks all AST nodes', function(done) {
-    var ast = acorn.parse('var foo = function(a) {}, bar = {a: 7}, c = 2+2;');
+    var ast = acorn.parse(code, {
+      ecmaVersion: 6
+    });
     var nodeTypes = [];
     walk.simple(ast, walkall.makeVisitors(function(node) {
       nodeTypes.push(node.type);
     }), walkall.traversers);
-    nodeTypes.should.eql([
+    nodeTypes.should.eql( [
+      'Literal',
+      'Identifier',
+      'ImportSpecifier',
+      'ImportDeclaration',
       'Identifier',
       'BlockStatement',
       'Identifier',
@@ -17,15 +24,32 @@ describe('walkall', function() {
       'Identifier',
       'Identifier',
       'Literal',
+      'Property',
       'ObjectExpression',
       'VariableDeclarator',
+      'VariableDeclaration',
       'Identifier',
-      'Literal',
-      'Literal',
-      'BinaryExpression',
+      'Identifier',
+      'Property',
+      'ObjectPattern',
+      'Identifier',
       'VariableDeclarator',
       'VariableDeclaration',
-      'Program',
+      'BlockStatement',
+      'Identifier',
+      'FunctionDeclaration',
+      'ExportDeclaration',
+      'Identifier',
+      'Identifier',
+      'ArrowFunctionExpression',
+      'Literal',
+      'Literal',
+      'ArrayExpression',
+      'Identifier',
+      'MemberExpression',
+      'CallExpression',
+      'ExpressionStatement',
+      'Program'
     ]);
     done();
   });
